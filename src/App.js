5d2ce737798feeea3e91452386c5a805d2eab3cc
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-31 20:43:45 +0200

    application logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import LoginForm from './LoginForm'
 import SignUpForm from './SignUpForm'
-
 import React, { Component } from 'react'
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import Axios from "axios"
+import { basePath, getConfig } from "./util"
 
 export default class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      todos: []
     }
   }
 
@@ -17,6 +21,7 @@ export default class App extends Component {
     const user = JSON.parse(localStorage.getItem("user"))
     if (user && user.token) {
       this.setLoggedIn(true)
+      this.fetchTodos()
     }
   }
 
@@ -24,20 +29,34 @@ export default class App extends Component {
     this.setState({
       loggedIn: hasLoggedIn
     })
+    this.fetchTodos()
   }
 
   logout() {
-    this.setLoggedIn(false)
+    this.setState({
+      loggedIn: false,
+      todos: []
+    })
     localStorage.removeItem("user")
   }
+
+  fetchTodos() {
+    console.log("Fetching todos again...")
+    Axios.get(`${basePath}/todos`, getConfig())
+      .then(res => this.setState({
+        todos: res.data
+      }))
+  }
+
 
 
   render() {
     if (this.state.loggedIn) {
       return (
         <div>
-          <div>Welcome, here u can add todos</div>
-          <button type="button" onClick={() => this.logout()}>Logout</button>
+          <TodoForm fetchTodos={this.fetchTodos.bind(this)} />
+          <TodoList todos={this.state.todos} />
+          <button id="logout" type="button" onClick={() => this.logout()}>Logout</button>
         </div>
       )
     } else {
@@ -46,7 +65,7 @@ export default class App extends Component {
           <h1>Login</h1>
           <LoginForm setLoggedIn={this.setLoggedIn.bind(this)} />
           <h1>Sign up</h1>
-          <SignUpForm />
+          <SignUpForm setLoggedIn={this.setLoggedIn.bind(this)} />
         </div>
       )
     }
