e6a70ca4afed729e1146b7cdb98dd8cf0ff2863b
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-31 20:42:50 +0200

    added getconfig for headers

--- src/util.js
+++ src/util.js
@@ -1,1 +1,16 @@
 export const basePath = process.env.NODE_ENV === "production" ? "https://sap-api-gateway.herokuapp.com" : "http://localhost:3003"
+
+
+
+export const getConfig = () => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    if (user) {
+        return {
+            headers: { Authorization: `Bearer ${user.token}` }
+        }
+    } else {
+        return {
+
+        }
+    }
+}
